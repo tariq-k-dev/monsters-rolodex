b8f3c6427062dfa0954cfa7b164a76c122451aa1
Tariq Khan <digitalgnomes@gmail.com>
2020-04-13 00:00:09 -0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
+import './App.styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     this.setState({ monsters: data });
+    //   })
+    //   .catch((error) => {
+    //     console.log('There was error in the API call 😂');
+    //   });
+
+    // ES7 async/await
+    (async () => {
+      try {
+        const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await usersResponse.json();
+        this.setState({ monsters: users });
+      } catch(error) {
+        console.log('There was error in the API call 😂');
+      }
+
+    })();
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="app">
+        <h1 id="app-h1">Monsters Rolodex</h1>
+        <SearchBox
+          placeholder={'search monsters'}
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
