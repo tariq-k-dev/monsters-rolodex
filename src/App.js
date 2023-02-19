94e3f73a330fe74f530a78aab7f74b39f324a374
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2023-02-19 12:37:01 -0800

    Refactor App component with hooks

--- src/App.js
+++ src/App.js
@@ -1,70 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 import './App.styles.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([monsters]);
+  const [searchField, setSearchField] = useState('');
+  const onSearchChange = e => {
+    setSearchField(e.target.value);
+  };
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-  }
-
-  componentDidMount() {
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     this.setState(() => { monsters: data });
-    //   })
-    //   .catch((error) => {
-    //     console.log('There was error in the API call 😂');
-    //   });
-
-    // ES7 async/await
+  // Get initial list of monsters, and only update if monsters changes
+  useEffect(() => {
     (async () => {
       try {
         const usersResponse = await fetch(
           'https://jsonplaceholder.typicode.com/users'
         );
         const users = await usersResponse.json();
-        this.setState(() => {
-          return { monsters: users };
-        });
+        setMonsters(users);
       } catch (error) {
         console.log('There was error in the API call 😂');
       }
     })();
-  }
+  }, [monsters]);
 
-  onSearchChange = e => {
-    const searchField = e.target.value;
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters.filter(monster =>
+  // Update filtered monsters list based on search input changes
+  setFilteredMonsters(
+    monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+    )
+  );
 
-    return (
-      <div className="app">
-        <h1 id="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          placeholder={'Search monsters'}
-          onChangeHandler={onSearchChange}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <h1 id="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        placeholder={'Search monsters'}
+        onChangeHandler={onSearchChange}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/AppClass.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import './App.styles.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  componentDidMount() {
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     this.setState(() => { monsters: data });
+    //   })
+    //   .catch((error) => {
+    //     console.log('There was error in the API call 😂');
+    //   });
+
+    // ES7 async/await
+    (async () => {
+      try {
+        const usersResponse = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        const users = await usersResponse.json();
+        this.setState(() => {
+          return { monsters: users };
+        });
+      } catch (error) {
+        console.log('There was error in the API call 😂');
+      }
+    })();
+  }
+
+  onSearchChange = e => {
+    const searchField = e.target.value;
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="app">
+        <h1 id="app-title">Monsters Rolodex</h1>
+        <SearchBox
+          placeholder={'Search monsters'}
+          onChangeHandler={onSearchChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
