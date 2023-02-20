14badeba18078f63d193ed7c0614b8433384385c
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2023-02-19 17:09:03 -0800

    Updates

---
+++ precache-manifest.26d9f530d83a47fe8ac9d78c1a00e7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbbfecf47b57be7ffdef97b6ce081285",
+    "url": "/tariq-k-dev/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "064bf5887d2580e099bd",
+    "url": "/tariq-k-dev/monsters-rolodex/static/css/main.73e224b8.chunk.css"
+  },
+  {
+    "revision": "d5f77de2379ff389782c",
+    "url": "/tariq-k-dev/monsters-rolodex/static/js/2.9c39fcd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tariq-k-dev/monsters-rolodex/static/js/2.9c39fcd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064bf5887d2580e099bd",
+    "url": "/tariq-k-dev/monsters-rolodex/static/js/main.af0eda94.chunk.js"
+  },
+  {
+    "revision": "d7a6bf12adeca8827bdf",
+    "url": "/tariq-k-dev/monsters-rolodex/static/js/runtime-main.cbc156de.js"
+  }
+]);
