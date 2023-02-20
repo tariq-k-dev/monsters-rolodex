6be6b5e9027c7ea5deed1d402f5f5f8b8ac90534
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2023-02-19 20:05:24 -0800

    Fix CardList key warning

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ import Card from '../card/card.component';
 const CardList = ({ monsters }) => (
   <div className="card-list">
     {monsters.map(monster => (
-      <Card key={monster.id} monster={monster} />
+      <Card key={`${monster.id}`} monster={monster} />
     ))}
   </div>
 );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ const Card = ({ monster }) => {
   const { id, name, email } = monster;
 
   return (
-    <div className="card-container">
+    <div key={id} className="card-container">
       <img
         src={`https://robohash.org/${id}?set=set2&size=180x180`}
         alt="monster"
